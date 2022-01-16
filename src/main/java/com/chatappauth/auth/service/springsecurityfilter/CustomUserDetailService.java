cff5ee75b73cfb8a04ed485c375aa7ceffb70d01
Iuri de Lima Marques <iurilmarques@hotmail.com>
2022-01-16 20:21:35 -0300

    master - minor fix at the authentication

--- src/main/java/com/chatappauth/auth/service/springsecurityfilter/CustomUserDetailService.java
+++ src/main/java/com/chatappauth/auth/service/springsecurityfilter/CustomUserDetailService.java
@@ -33,8 +33,8 @@ public class CustomUserDetailService implements UserDetailsService {
             LocalDateTime dateExpiration = Instant.ofEpochMilli(expirantionMilli).atZone(ZoneId.systemDefault()).toLocalDateTime();
 
             loggedUser = new UserPrincipal(user, dateExpiration);
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
+        } catch (UsernameNotFoundException e) {
+            throw new RuntimeException("Username not found");
         }
 
         return loggedUser;
