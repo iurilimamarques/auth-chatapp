6c5529792742ebcaaa1435abf7d39c7f4b5cd85c
Iuri de Lima Marques <iurilmarques@hotmail.com>
2022-03-20 21:35:57 -0300

    master - yml config

--- src/main/java/com/chatappauth/auth/config/SpringConfig.java
+++ src/main/java/com/chatappauth/auth/config/SpringConfig.java
@@ -9,7 +9,10 @@ import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Duration;
+import java.util.Objects;
 
 @Configuration
 public class SpringConfig {
@@ -26,13 +29,26 @@ public class SpringConfig {
     @Value("${spring.redis.password}")
     private String password;
 
+    @Value("${spring.redis.url}")
+    private String url;
+
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
+    public JedisConnectionFactory jedisConnectionFactory() throws URISyntaxException {
+        String environment = System.getenv("ENVIRONMENT");
+
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setDatabase(database);
-        redisStandaloneConfiguration.setPassword(RedisPassword.none());
+        if (Objects.isNull(environment) || !environment.equals("prod")) {
+            redisStandaloneConfiguration.setHostName(host);
+            redisStandaloneConfiguration.setPort(port);
+            redisStandaloneConfiguration.setDatabase(database);
+            redisStandaloneConfiguration.setPassword(RedisPassword.none());
+        } else {
+            URI redisURI = new URI(url);
+            redisStandaloneConfiguration.setHostName(redisURI.getHost());
+            redisStandaloneConfiguration.setPort(redisURI.getPort());
+            redisStandaloneConfiguration.setDatabase(database);
+            redisStandaloneConfiguration.setPassword(redisURI.getUserInfo().split(":", 2)[1]);
+        }
 
         JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfigurationBuilder = JedisClientConfiguration.builder();
         jedisClientConfigurationBuilder.connectTimeout(Duration.ofSeconds(60));
@@ -40,7 +56,7 @@ public class SpringConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate() throws URISyntaxException {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
