912c7e2507c9d26224d65a119c195b6beb3ebd58
Iuri de Lima Marques <iurilmarques@hotmail.com>
2023-01-14 22:40:07 -0300

    master - enabling repositories

--- src/main/java/com/chatappauth/auth/AuthApplication.java
+++ src/main/java/com/chatappauth/auth/AuthApplication.java
@@ -4,10 +4,14 @@ import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EntityScan("com.chatcomponents")
 @EnableEmailTools
+@EnableJpaRepositories(basePackages = "com.chatappauth.auth.repository")
+@EnableRedisRepositories(basePackages = "com.chatappauth.auth.blacklist")
 public class AuthApplication {
 
 	public static void main(String[] args) {
