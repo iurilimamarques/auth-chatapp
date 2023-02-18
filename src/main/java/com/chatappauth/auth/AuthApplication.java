b53aaf603a04c4c4f30be73fbbc7bd2e2c3ee8ed
Iuri de Lima Marques <iurilmarques@hotmail.com>
2023-02-18 12:12:45 -0300

    master

--- src/main/java/com/chatappauth/auth/AuthApplication.java
+++ src/main/java/com/chatappauth/auth/AuthApplication.java
@@ -4,8 +4,13 @@ import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EntityScan("com.chatcomponents")
@@ -16,5 +21,23 @@ public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(true);
+
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+
+		for (HttpMethod httpMethod : HttpMethod.values()) {
+			config.addAllowedMethod(httpMethod);
+		}
+
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
