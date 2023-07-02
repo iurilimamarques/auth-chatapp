9f817d8fbcaba9899a3b7889b7639b189b39a49e
Iuri de Lima Marques <iuridelimamarques@jvrllh3.betha.com.br>
2023-07-02 18:35:35 -0300

    master - fix redis configuration

--- src/main/java/com/chatappauth/auth/config/SpringConfig.java
+++ src/main/java/com/chatappauth/auth/config/SpringConfig.java
@@ -3,15 +3,17 @@ package com.chatappauth.auth.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
+import java.util.logging.Logger;
 
 @Configuration
 public class SpringConfig {
+    static Logger logger = Logger.getLogger(SpringConfig.class.getName());
 
     @Value("${spring.redis.database}")
     private int database;
@@ -21,19 +23,28 @@ public class SpringConfig {
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(url);
-        redisStandaloneConfiguration.setDatabase(database);
-
-        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfigurationBuilder = JedisClientConfiguration.builder();
-        jedisClientConfigurationBuilder.connectTimeout(Duration.ofSeconds(60));
-        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfigurationBuilder.build());
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName(url);
+        connectionFactory.setUsePool(true);
+        connectionFactory.setPort(6379);
+        connectionFactory.getPoolConfig().setMaxTotal(10);
+        connectionFactory.getPoolConfig().setMaxIdle(10);
+        return connectionFactory;
     }
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        if (null == redisConnectionFactory) {
+            logger.warning("Redis Template Service is not available");
+            return null;
+        }
+
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
+        template.setEnableTransactionSupport(true);
         return template;
     }
 }
